da8b12a4193b3a9f796ea9a980811cbb0e699a48
Narongded <32951598+Narongded@users.noreply.github.com>
2021-01-18 20:31:11 +0700

    Update regis.js

--- src/routes/regis.js
+++ src/routes/regis.js
@@ -8,19 +8,20 @@ const router = express.Router();
 
 router.post('/', async (req, res, next) => {
     console.log(req.body)
-    const sql = "INSERT INTO alluser (firstname, lastname,email,password,role) VALUES ?";
+    const sql = "INSERT INTO alluser (firstname, lastname,studentnumber,email,password,role) VALUES ?";
     const values = [
         [
             `${req.body.first_name}`,
             `${req.body.last_name}`,
+            `${req.body.studentnumber}`,
             `${req.body.email}`,
             `${bcrypt.hashSync(req.body.password, 10)}`,
             `${req.body.role}`,
         ]
     ];
     con.query(sql, [values], function (err, result) {
-        if (err) throw err;
-        console.log("Number of records inserted: " + result.affectedRows);
+        if (err) return res.status(400).json({ mes: 'Email หรือ รหัสนักศึกษามีอยู๋ในระบบแล้ว' });
+        return res.status(200).json({mes : 'Success'})
     });
 })
 export default router
