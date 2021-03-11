e6a16494724f4cee08c43759da4839fdd5f9ae56
Narongded <32951598+Narongded@users.noreply.github.com>
2021-03-12 03:18:35 +0700

    api savefile

--- src/routes/user.js
+++ src/routes/user.js
@@ -27,12 +27,14 @@ router.get('/getfile-lecture/:pdfid', (req, res, next) => {
     });
 })
 router.get('/getdata-lecture/:uid/:cid', (req, res, next) => {
-    const sql = `SELECT * ,  studentpdf.alluser_uid as studentid
+    const sql = `SELECT * , GROUP_CONCAT(CONCAT('\[\',file.filename,'\,\',file.type,'\]\') SEPARATOR '|') AS file, studentpdf.alluser_uid as studentid
     FROM studentpdf
     INNER JOIN pdf on studentpdf.teacherpdf_tpid = pdf.tpid
     INNER JOIN chapter on chapter.cid = pdf.chapter_cid
+    LEFT JOIN file on file.pdfid = studentpdf.sid
     WHERE studentpdf.alluser_uid = "${req.params.uid}" and chapter.cid = ${req.params.cid}`;
     con.query(sql, (err, result, field) => {
+        console.log(result[0].file.split("|").length)
         res.status(200).json({ data: result, status: 'Success' })
     });
 })
@@ -50,13 +52,50 @@ router.get('/getchapter/:uid', (req, res, next) => {
     });
 })
 
+
+router.post('/upload-file/:pdfid', (req, res, next) => {
+    const sql1 = `SELECT * FROM file
+    WHERE pdfid = "${req.params.pdfid}"
+    and type = "${req.body.type}"`
+    con.query(sql1, (err, result, field) => {
+        if (err || result.length === 0) {
+            const sql = "INSERT INTO file (pdfid,filename,type) VALUES ?";
+            const namefile = req.files.file.mimetype.includes("-")
+                ? uid() + "." + req.files.file.mimetype.split("-")[1]
+                : uid() + "." + req.files.file.mimetype.split("/")[1]
+            const values = [
+                [
+                    `${req.params.pdfid}`,
+                    `${namefile}`,
+                    `${req.body.type}`
+                ]
+            ];
+            con.query(sql, [values], (err, insertresult, field) => {
+                if (insertresult.length === 0) return res.status(400).json({ status: 'failed wrong data' })
+                const filepdf = req.files.file
+                filepdf.mv(path.join(path.resolve(), '/src/public/file/') + namefile);
+                return res.status(200).json({ status: 'Success' })
+            });
+        }
+        else {
+            const filepdf = req.files.file
+            fs.unlinkSync(path.join(path.resolve(), '/src/public/file/') + result[0].filename)
+            filepdf.mv(path.join(path.resolve(), '/src/public/file/') + result[0].filename)
+            return res.status(200).json({ status: 'Success' })
+        }
+    });
+
+
+
+
+})
+
 router.post('/upload-studentpdf', (req, res, next) => {
     if (req.files === null) return res.status(400).json({ status: 'File PDF not found' })
     const sql = `SELECT * FROM studentpdf
     WHERE alluser_uid = "${req.body.userid}"
     and teacherpdf_tpid = "${req.body.teacherpdf_tpid}"`
     con.query(sql, (err, result, field) => {
-        console.log(err)
         if (err || result.length === 0) {
             const namefile = uid() + '.pdf';
             const filepdf = req.files.file
