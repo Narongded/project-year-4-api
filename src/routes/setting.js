93e465e1f393584f6d824ca4bfef338ceaeed59d
Narongded <32951598+Narongded@users.noreply.github.com>
2021-04-09 00:49:13 +0700

    เพิ่ม api setting

--- src/routes/setting.js
+++ src/routes/setting.js
@@ -7,9 +7,27 @@ import generator from 'generate-password'
 import { adminAuth, permit } from '../service/passportAdmin.js';
 import { upload } from '../service/handlefile.js'
 import con from '../manage/connectdb.js'
+import nodemailer from 'nodemailer'
 const router = express.Router();
 const app = express();
 //-------------------------------------Setting----------------------------------
+
+
+
+router.get('/loadSetting/:uid', (req, res, next) => {
+
+    const sql = `SELECT * FROM lecturesharetoggle WHERE alluser_uid = "${req.params.uid}"`
+    con.query(sql, (err, result, field) => {
+        if (err) return res.status(400).json({ status: 'failed wrong data' })
+        const sql = `SELECT * FROM lecturesharedlist WHERE lecturesharetoggle_sharedtoggleid = ${result[0].sharedtoggleid}`
+        con.query(sql, (err, result2, field) => {
+            if (err) return res.status(400).json({ status: 'failed wrong data' })
+            res.status(200).json({ data: result2, statusshared: result, status: 'Success' })
+        });
+    });
+})
+
+
 router.put('/shared/:uid', (req, res, next) => {
     const sql = `UPDATE lecturesharetoggle set status = ${req.body.status} where alluser_uid = "${req.params.uid}" `;
     con.query(sql, (err, result) => {
@@ -19,37 +37,48 @@ router.put('/shared/:uid', (req, res, next) => {
     });
 })
 
-router.put('/update-chapter/:chapterid', (req, res, next) => {
-    const sql = `UPDATE chapter SET name = ? WHERE cid = ${req.params.chapterid}`;
+router.post('/add-people/:listid', (req, res, next) => {
+    const sql = `insert into lecturesharedlist (lecturesharetoggle_sharedtoggleid,alluser_uid) VALUES ?`;
     const values = [
         [
-            `${req.body.chaptername}`,
+            `${req.params.listid}`,
+            `${req.body.email}`
         ]
     ];
     con.query(sql, [values], (err, result) => {
         if (err || result.length === 0) return res.status(400).json({ status: 'failed wrong data' })
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'slackdevtool002@gmail.com',
+                pass: 'jackkubpom55'
+            }
+        });
+
+        var mailOptions = {
+            from: '60070023@it.kmitl.ac.th',
+            to: '60070023@it.kmitl.ac.th',
+            subject: 'Email From System ',
+            text: `คุณถูกรับเชิญเข้าสู่เลคเชอร์โน๊ตของคุณ ${req.body.owner} ลิงค์ http://localhost:3000/student-chapter/${req.body.owner}`
+        };
+        transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email sent: ' + info.response);
+            }
+        });
         res.status(200).json({ status: 'Success' })
     });
 })
 
-router.delete('/delete-chapter/:chapterid', (req, res, next) => {
-    const sql = `DELETE FROM chapter WHERE cid=${req.params.chapterid}`;
+router.delete('/delete-people/:sharedlistid', (req, res, next) => {
+    const sql = `DELETE FROM lecturesharedlist WHERE sharedlistid=${req.params.sharedlistid}`;
 
     con.query(sql, (err, result) => {
-
+        console.log(err)
         if (err || result.length === 0) return res.status(400).json({ status: 'failed wrong data' })
         res.status(200).json({ status: 'Success' })
-    });
-})
-
-router.get('/getfile-pdf/:chapterid', (req, res, next) => {
-
-    const sql = `SELECT * FROM pdf WHERE chapter_cid = ${req.params.chapterid} `;
-
-    con.query(sql, (err, result, field) => {
-
-        if (err) return res.status(400).json({ status: 'failed wrong data' })
-        res.status(200).json({ data: result, status: 'Success' })
     });
 })
 
