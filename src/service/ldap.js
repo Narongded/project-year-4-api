58db321d683358c4af4a5316803066a2a9767e6f
naive555 <non.naive@gmail.com>
2021-05-26 19:23:28 +0700

    ลองแก้ ldap

--- src/service/ldap.js
+++ src/service/ldap.js
@@ -1,4 +1,4 @@
-
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 import ldapjs from 'ldapjs'
 import dotenv from 'dotenv'
@@ -10,27 +10,32 @@ const loginLdap = (upn, password) => new Promise(async (resolve, reject) => {
     await client.bind(`${upn}@it.kmitl.ac.th`, password, (error) => {
         if (error) {
             reject(error)
-            client.unbind()
-            client.destroy()
+            client.destroy(err => {
+                console.error("error: " + err);
+            })
         }
     });
     await client.search(process.env.BASEDN, searchOptions, (error, res) => {
         let data = {}
         if (error) {
             reject(error)
-            client.unbind()
-            client.destroy()
+            client.destroy(err => {
+                console.error("error: " + err);
+            })
         }
         res.on("searchEntry", (entry) => {
             data = entry.object
         })
         res.on("error", function (err) {
-            console.error("error: " + err.message);
+            client.destroy(err => {
+                console.error("error: " + err);
+            })
         });
         res.on("end", function (result) {
             resolve(data)
-            client.unbind()
-            client.destroy()
+            client.unbind(callback => {
+                console.error("error: " + callback);
+            })
         });
     })
 })
